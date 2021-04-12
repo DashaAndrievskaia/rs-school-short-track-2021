6647602f7f30592a4e6a25a7e757d7fde3de1620
Daria Andrievskaia <andrievskaya.dd@gmail.com>
2021-04-12 16:57:40 +0200

    feat: solved problem number 11

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,27 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  let result = '';
+  let groupLetter;
+  let groupCount = 0;
+  for (let i = 0; i <= str.length; i++) {
+    const letter = str[i];
+    if (groupLetter === letter) {
+      groupCount += 1;
+    } else {
+      if (groupCount > 0) {
+        if (groupCount > 1) {
+          result += groupCount;
+        }
+        result += groupLetter;
+      }
+      groupCount = 1;
+      groupLetter = letter;
+    }
+  }
+
+  return result;
 }
 
 module.exports = encodeLine;
